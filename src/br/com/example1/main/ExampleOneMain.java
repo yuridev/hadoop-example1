238aad17a4b4063b6496b7a9a6370dbc34a84d1e
dev28 <yuri@megasoftgyn.com.br>
2017-05-19 14:49:31 -0400

    First commit

---
+++ src/br/com/example1/main/ExampleOneMain.java
@@ -0,0 +1,8 @@
+package br.com.example1.main;
+
+public class ExampleOneMain {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/br/com/example1/map/Map.java
@@ -0,0 +1,17 @@
+package br.com.example1.map;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] row = value.toString().split("\t");
+        context.write(new Text(row[2]), new IntWritable(1));
+    }
+}
